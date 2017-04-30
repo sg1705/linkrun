c720fae69279d0fa543ff6ddb7bb8bbb289f4c3d
Saurabh Gupta <sg1705@gmail.com>
2017-04-30 18:43:36 -0400

    fixed routes and moved over to angular app

---
+++ server/routes/api-route.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const express = require('express');
+const config = require('config');
+var path = require('path');
+
+const router = express.Router();
+
+// api
+router.use('/users', require('../model/user.api'));
+router.use('/linksv2', require('../api/link.api'));
+
+module.exports = router;
