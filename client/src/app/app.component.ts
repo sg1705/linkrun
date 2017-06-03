c014ba265e6da3b2ca3636b389ff39b543fd650c
wei2learn <wei2learn@gmail.com>
2017-06-03 10:39:11 -0400

    small fix

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -19,10 +19,10 @@ export class AppComponent {
       if (!user.id) {
         window.location.href = '/_/';
       }
-      ga('set', 'userId', user.id); // Set the user ID using signed-in user_id.
       ga('create', 'UA-99455967-1', 'auto');
       this.router.events.subscribe(event => {
         if (event instanceof NavigationEnd) {
+          ga('set', 'userId', user.id); // Set the user ID using signed-in user_id.          
           ga('set', 'page', event.urlAfterRedirects);
           ga('send', 'pageview');
         }
