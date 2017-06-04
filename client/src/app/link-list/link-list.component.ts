92df52130ba8fc45ddd2105162023826a136a3a0
Saurabh Gupta <sg1705@gmail.com>
2017-06-04 08:56:32 -0400

    added typing animation on login page (#54)
    * cache if production or staging
    
    * fixed double calling of /users endpoint
    
    * using helmet to do security
    
    * added typing animation to the home page
    
    * added secondary callout
    
    * fixed bug in prod for rotator
    
    * removed the tabs in header

--- client/src/app/link-list/link-list.component.ts
+++ client/src/app/link-list/link-list.component.ts
@@ -26,12 +26,7 @@ export class LinkListComponent implements OnInit {
     this.userService.getCurrentUser().then(user => {
       this.user = user;
     }).then(_ => {
-      this.linkService.getLinks().then(links => {
-        this.links = links;
-        links.forEach(link => {
-          link['canEdit'] = (link.userId == this.user.id);
-        });
-      })
+      this.refreshList();
     })
     .catch(err => {
       console.log(err);
@@ -44,6 +39,15 @@ export class LinkListComponent implements OnInit {
         this.ngOnInit();
       }
     })
+  }
+
+  refreshList() {
+      return this.linkService.getLinks().then(links => {
+        this.links = links;
+        links.forEach(link => {
+          link['canEdit'] = (link.userId == this.user.id);
+        });
+      })
   }
 
 }
