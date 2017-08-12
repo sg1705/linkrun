c7100c0c1bafcdc3aeb6dab58872c7404e6aa98a
Saurabh Gupta <sg1705@gmail.com>
2017-08-12 11:39:57 -0400

    added an org lookup when a user signs in

--- server/model/user.api.js
+++ server/model/user.api.js
@@ -5,10 +5,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const config = require('config');
 const UserService = require(`./user.js`);
+const OrgService = require(`./org.js`);
 const cookieService = require('../cookie.js');
 const Datastore = require('@google-cloud/datastore');
 
 const userService = new UserService();
+const orgService = new OrgService();
 
 function getModel () {
   return require(`./model-${config.get('db.DATA_BACKEND')}`)();
@@ -27,18 +29,24 @@ router.use(bodyParser.json());
 router.get('/', (req, res, next) => {
   //get user from cookie
   var userId = cookieService.getXsession(req).userId;
+  //create user object
+  var userData = {};
+  
   userService.getUser(userId)
   .then(entity => {
-    //create user object
-    var userData = {};
     userData['id'] = entity[Datastore.KEY].id;
     userData["orgId"] = entity['orgId'];
     userData["email"] = entity['email'];
     userData["fName"] = entity['fName'];
     userData["lName"] = entity['lName'];
     userData["picture"] = entity['picture'];
-    res.json(userData);
-  }).catch(err => {
+    return orgService.getOrg(userData['orgId']);
+  })
+  .then(orgEntity => {
+    userData['orgName'] = orgEntity.orgName;
+    res.json(userData);    
+  })
+  .catch(err => {
       console.log(err);
       res.clearCookie("userId");
       res.clearCookie("orgId");
