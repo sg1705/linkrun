0af4dd89b07a05c26d0ecccefe2952abae0bb314
Saurabh Gupta <sg1705@gmail.com>
2017-08-12 11:42:37 -0400

    added domain to the header

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { LoginComponent } from './login/login.component';
 import { UserService } from './services/user.service';
+import { User } from './model/user';
 
 @Component({
   selector: 'app-root',
@@ -9,9 +10,12 @@ import { UserService } from './services/user.service';
 })
 export class AppComponent {
 
+  user: User;
+
   constructor(private userService: UserService) {
     this.userService.getCurrentUser().then(user => {
       console.debug('logged in user', user);
+      this.user = user;
       if (!user.id) {
         window.location.href = '/_/';
       }
