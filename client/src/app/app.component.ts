96c0886f167ab6ba8d8e78cf63c406d8af2fbc9d
Saurabh Gupta <sg1705@gmail.com>
2017-10-07 12:23:25 -0400

    added analytics tracking for client app and static files

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,13 +1,15 @@
-/// <reference path="../../node_modules/@types/google.analytics/index.d.ts" />
-
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from "@angular/router";
+
 import { LoginComponent } from './login/login.component';
 import { UserService } from './services/user.service';
-import { User } from './model/user';
-import { Observable } from 'rxjs/Rx';
-import 'rxjs/Rx';
-import { Router, NavigationEnd } from "@angular/router";
 import { GoogleAnalyticsEventsService } from "./services/google-analytics-events.service";
+import { User } from './model/user';
+import { environment } from '../environments/environment';
+
+import 'rxjs/Rx';
+import { Observable } from 'rxjs/Rx';
+
 
 @Component({
   selector: 'app-root',
@@ -18,7 +20,10 @@ export class AppComponent {
 
   user: Observable<User>;
 
-  constructor(private userService: UserService, public router: Router, public googleAnalyticsEventsService: GoogleAnalyticsEventsService) {
+  constructor(
+    private userService: UserService,
+    public router: Router,
+    private googleAnalyticsEventsService: GoogleAnalyticsEventsService) {
   }
 
   ngOnInit() {
@@ -27,17 +32,21 @@ export class AppComponent {
       console.debug('logged in user', user);
       if (!user.id) {
         window.location.href = '/_/';
-      }  
-      ga('create', 'UA-99498648-1', 'auto');
-      this.router.events.subscribe(event => {
-        if (event instanceof NavigationEnd) {
+      }
+      console.log('Environment is production:', environment.production);
+
+    })
+
+    ga('create', environment.ga.GA_TRACKING_ID, 'auto');
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.user.subscribe(user => {
           ga('set', 'userId', user.id); // Set the user ID using signed-in user_id.          
           ga('set', 'page', event.urlAfterRedirects);
           ga('send', 'pageview');
-        }
-      });    
-    })
-   
+        })
+      }
+    });
   }
 
 }
