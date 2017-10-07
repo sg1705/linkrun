5cf49954ef635888a673569a891eb7350c61148c
saurabhagarwal <saurabh.wl@gmail.com>
2017-10-07 08:34:05 -0400

    adding google analytics

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -3,11 +3,12 @@
 import { Component } from '@angular/core';
 import { LoginComponent } from './login/login.component';
 import { UserService } from './services/user.service';
-import 'rxjs/Rx';
-import { Observable } from 'rxjs/Rx';
 import { User } from './model/user';
+import { Observable } from 'rxjs/Rx';
+import 'rxjs/Rx';
 import { Router, NavigationEnd } from "@angular/router";
 import { GoogleAnalyticsEventsService } from "./services/google-analytics-events.service";
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -16,15 +17,17 @@ import { GoogleAnalyticsEventsService } from "./services/google-analytics-events
 export class AppComponent {
 
   user: Observable<User>;
+
   constructor(private userService: UserService, public router: Router, public googleAnalyticsEventsService: GoogleAnalyticsEventsService) {
   }
+
   ngOnInit() {
-    this.user = Observable.fromPromise(this.userService.getCurrentUser());
+    this.user = this.userService.getCurrentUser();
     this.user.subscribe(user => {
       console.debug('logged in user', user);
       if (!user.id) {
         window.location.href = '/_/';
-      }
+      }  
       ga('create', 'UA-99498648-1', 'auto');
       this.router.events.subscribe(event => {
         if (event instanceof NavigationEnd) {
@@ -32,10 +35,8 @@ export class AppComponent {
           ga('set', 'page', event.urlAfterRedirects);
           ga('send', 'pageview');
         }
-      });
-    })
-
-
+      });    
+    })   
   }
 
 }
