665f18b61db99772943d4db28465544b372a7091
Wei Zhou <wei2learn@gmail.com>
2017-11-01 00:05:37 -0400

    added a test with stub

---
+++ server/model/org.spec.js
@@ -0,0 +1,25 @@
+'use strict';
+const sinon = require('sinon');
+const chai = require('chai');
+const sinonChai = require('sinon-chai');
+const should = chai.should();
+chai.use(sinonChai);
+const proxyquire = require('proxyquire');
+const ModelService = require('./model-service');
+const Org = require('./org')
+let org = new Org();
+
+describe('org', function() {
+    context('createOrg', function() {
+        let msCreateStub = sinon.stub(ModelService.prototype, 'create');
+        it('should call modelService.create with correct data', function(){
+            let ms = new ModelService('Org');
+            const ORG_NAME = 'a';
+            const ORG_TYPE = 'b';
+            const orgData = {orgName: ORG_NAME, orgType: ORG_TYPE};
+            org.createOrg(ORG_NAME, ORG_TYPE);
+            msCreateStub.should.have.been.deep.calledWith(orgData);
+        })
+
+    })
+})
