87b1bdb6912f7436460e0f20a25c6ff20174fa28
Wei Zhou <wei2learn@gmail.com>
2017-11-02 23:10:18 -0400

    added more tests in org.js

--- server/model/org.spec.js
+++ server/model/org.spec.js
@@ -22,10 +22,59 @@ describe('org', function() {
             let ms = new ModelService('Org');
             const ORG_NAME = 'a';
             const ORG_TYPE = 'b';
-            const orgData = {orgName: ORG_NAME, orgType: ORG_TYPE};
+            const ORG_DATA = {orgName: ORG_NAME, orgType: ORG_TYPE};
             org.createOrg(ORG_NAME, ORG_TYPE);
-            msCreateStub.should.have.been.deep.calledWith(orgData);
+            msCreateStub.should.have.been.calledWith(ORG_DATA);
         })
 
     })
+
+    context('addOrgShortName', function() {
+        let msUpdateStub;
+        before(function(){
+            msUpdateStub = sinon.stub(ModelService.prototype, 'update');
+        })
+        after(function(){
+            msUpdateStub.restore();
+        })
+        it('should call modelService.update with correct data', function(){
+            let ms = new ModelService('Org');
+            const ORG_ID = '0';
+            const ORG_NAME = 'a';
+            const ORG_SHORT_NAME = 'b';
+            const IS_PUBLIC_LINKS_ALLOWED = 'c';
+            const ORG_TYPE = 'd';
+            const ORG_DATA = {
+                orgName: ORG_NAME, 
+                orgShortName: ORG_SHORT_NAME, 
+                isPublicLinksAllowed: IS_PUBLIC_LINKS_ALLOWED, 
+                orgType: ORG_TYPE
+            };
+            org.addOrgShortName(ORG_ID, ORG_NAME, ORG_SHORT_NAME, ORG_TYPE, IS_PUBLIC_LINKS_ALLOWED);
+            msUpdateStub.should.have.been.calledWith(ORG_ID, ORG_DATA);
+        })
+    })
+
+    context('updateOrg', function() {
+        let msUpdateStub;
+        before(function(){
+            msUpdateStub = sinon.stub(ModelService.prototype, 'update');
+        })
+        after(function(){
+            msUpdateStub.restore();
+        })
+        it('should call modelService.update with correct data', function(){
+            let ms = new ModelService('Org');
+            const ORG_ID = '0';
+            const ORG_NAME = 'a';
+            const ORG_TYPE = 'd';
+            const ORG_DATA = {
+                orgName: ORG_NAME, 
+                orgType: ORG_TYPE
+            };
+            org.updateOrg(ORG_ID, ORG_NAME, ORG_TYPE);
+            msUpdateStub.should.have.been.calledWith(ORG_ID, ORG_DATA);
+        })
+    })
+
 })
