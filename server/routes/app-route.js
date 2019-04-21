96babb228703465e80e59a97433d6ea10cc40cfc
sagupta <sg1705@gmail.com>
2019-04-21 14:36:26 -0400

    added a flag for upgrade

--- server/routes/app-route.js
+++ server/routes/app-route.js
@@ -9,9 +9,17 @@ const router = express.Router();
 // angular app
 
 router.get('/app*', function (req, res, next) {
-  res.sendFile(path.join(__dirname, '../../dist/index.html'));
+  if (config.get('flags.angular8') == 'true') {
+    res.sendFile(path.join(__dirname, '../../dist/client2/index.html'));  
+  } else {
+    res.sendFile(path.join(__dirname, '../../dist/index.html'));
+  }
 });
 
-router.use('/', express.static(path.join(__dirname, '../../dist')));
+if (config.get('flags.angular8') == 'true') {
+  router.use('/', express.static(path.join(__dirname, '../../dist/client2')));
+} else {
+  router.use('/', express.static(path.join(__dirname, '../../dist')));
+}
 
 module.exports = router;
