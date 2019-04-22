de9039891c8433948ef5e7f350a749799e7861d3
sagupta <sg1705@gmail.com>
2019-04-22 08:13:39 -0400

    added link model

---
+++ client2/src/app/model/link.ts
@@ -0,0 +1,25 @@
+export class Link {
+    id:           number;
+    link:         string;
+    url:          string;
+    description:  string;
+    userId:       number;
+    acl:          number = 0;
+  
+    static readonly LINK_ACL_DEFAULT = 0;
+    static readonly LINK_ACL_PUBLIC  = 1;
+    static readonly LINK_ACL_PRIVATE = 2;
+   
+    constructor(id, link, url, description, acl) {
+      this.id = id;
+      this.url = url;
+      this.link = link;
+      this.description = description;
+      this.acl = acl;
+    }
+  
+    setUserId(userId: number) {
+      this.userId = userId;
+    }
+    
+  }
