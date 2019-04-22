8c648d7f6956ef6c8b12cdb7502336ecb2d7e0f2
sagupta <sg1705@gmail.com>
2019-04-22 08:16:56 -0400

    added helper service

---
+++ client2/src/app/services/helper.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { User } from '../model/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HelperService {
+
+  constructor() { }
+
+  public generateFullShortLink(link:string) {
+    return 'https://link.run/' + link;
+  }
+
+  public generatePublicLink(link:string, user:User) {
+    if (user.orgAllowsPublic) {
+      return 'https://link.run/@' + user.orgShortName + '/' + link;
+    }
+    return null;
+  }
+
+  public generatePublicLinkFromOrgShortName(link:string, orgShortName:string) {
+      return 'https://link.run/@' + orgShortName + '/' + link;
+  }
+
+}
