b0fbaf8a5c5f63ee164020883140ecbcf52ce8e0
sagupta <sg1705@gmail.com>
2019-10-05 16:24:14 -0400

    added link-list module

--- client3/src/app/app.module.ts
+++ client3/src/app/app.module.ts
@@ -9,12 +9,13 @@ import { MaterialModule } from "./material/material.module";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { LoginComponent } from "./login/login.component";
+import { LinkListComponent } from "./link-list/link-list.component";
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent],
+  declarations: [AppComponent, LoginComponent, LinkListComponent],
   imports: [
-    BrowserModule,
     AppRoutingModule,
+    BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
     ReactiveFormsModule,
